40902ccd852966b83bffc3f442c3dd605a5b326e
Sam Jones <sam.jones240699@gmail.com>
2022-05-03 20:51:24 +0100

    added remapping from file

--- controllerInput.gd
+++ controllerInput.gd
@@ -4,24 +4,49 @@ var deadzone = 0.2
 var sensMult = 5
 var player : Node
 var input_swapped = false
+var CONFIG_NAME = "controller_bindings.json"
+
+var BUTTON_NAMES = {
+	"A": JOY_XBOX_A,
+	"B": JOY_XBOX_B,
+	"X": JOY_XBOX_X,
+	"Y": JOY_XBOX_Y,
+	"L1": JOY_L,
+	"R1": JOY_R,
+	"L2": JOY_L2,
+	"R2": JOY_R2,
+	"L3": JOY_L3,
+	"R3": JOY_R3,
+	"START": JOY_START,
+	"SELECT": JOY_SELECT,
+	"DPAD_UP": JOY_DPAD_UP,
+	"DPAD_DOWN": JOY_DPAD_DOWN,
+	"DPAD_LEFT": JOY_DPAD_LEFT,
+	"DPAD_RIGHT": JOY_DPAD_RIGHT,
+}
+
+var ACTION_SUBS = {
+	"pause":"ui_cancel"
+}
 
 var button_actions = {
-	JOY_START: "ui_cancel",
-	JOY_SELECT: "objective",
-	JOY_XBOX_A: "move_jump",
-	JOY_XBOX_B: "reload",
-	JOY_XBOX_X: "Interact",
-	JOY_XBOX_Y: "respawn",
-	JOY_DPAD_UP: "equipPogo",
-	JOY_DPAD_LEFT: "equipHammer",
-	JOY_DPAD_RIGHT: "equipCoin",
-	JOY_DPAD_DOWN: "equipDust",
-	JOY_R: "action",
-	JOY_R2: "action",
-	JOY_L: "altAction",
-	JOY_L2: "altAction",
-	JOY_L3: "move_sprint"
+	"ui_cancel": [JOY_START],
+	"objective": [JOY_SELECT],
+	"move_jump": [JOY_XBOX_A],
+	"reload": [JOY_XBOX_B],
+	"Interact": [JOY_XBOX_X],
+	"respawn": [JOY_XBOX_Y],
+	"equipPogo": [JOY_DPAD_UP],
+	"equipHammer": [JOY_DPAD_LEFT],
+	"equipCoin": [JOY_DPAD_RIGHT],
+	"equipDust": [JOY_DPAD_DOWN],
+	"action": [JOY_R, JOY_R2],
+	"altAction": [JOY_L, JOY_L2],
+	"move_sprint": [JOY_L3],
+	"dialogue_advance": [JOY_XBOX_A]
 }
+
+
 
 var action_queue = []
 
@@ -30,6 +55,8 @@ func _ready():
 	InputMap.action_set_deadzone("move_right", 0.2)
 	InputMap.action_set_deadzone("move_forward", 0.2)
 	InputMap.action_set_deadzone("move_backward", 0.2)
+
+	InputMap.add_action("dialogue_advance")
 
 	var ev = InputEventJoypadMotion.new()
 	ev.axis = JOY_AXIS_0
@@ -51,30 +78,78 @@ func _ready():
 	ev.axis_value = 1.0
 	InputMap.action_add_event("move_backward", ev)
 
-	for axis in button_actions:
-		ev = InputEventJoypadButton.new()
-		ev.button_index = axis
-		ev.pressed = true
-		InputMap.action_add_event(button_actions[axis], ev)
+	load_bindings()
+	
+	for action in button_actions:
+		for button in button_actions[action]:
+			ev = InputEventJoypadButton.new()
+			ev.button_index = button
+			ev.pressed = true
+			InputMap.action_add_event(action, ev)
 
 	ev = InputEventJoypadButton.new()
 	ev.button_index = JOY_BUTTON_1
 	InputMap.action_erase_event("ui_cancel", ev)
+
+func load_bindings():
+	var path = ""
+	if OS.has_feature("editor"):
+		path = "res://mods/" + CONFIG_NAME
+	else:
+		path = OS.get_executable_path().get_base_dir().plus_file("mods/" + CONFIG_NAME)
+	var file : File = File.new()
+	if file.open(path, File.READ) == OK:
+		var json = file.get_as_text()
+		var data = JSON.parse(json)
+		if data.error == OK:
+			if typeof(data.result) == TYPE_DICTIONARY:
+				for action in data.result:
+					if action == "sens_mult":
+						if typeof(data.result[action]) != TYPE_REAL or data.result[action] <= 0:
+							printerr("Value for sens_mult must be a positive number")
+							continue
+						sensMult = data.result[action]
+						continue
+					var subbed_action = action
+					if action in ACTION_SUBS:
+						subbed_action = ACTION_SUBS[action]
+					if not (subbed_action in button_actions):
+						printerr("Unknown action: " + action)
+						continue
+					if typeof(data.result[action]) == TYPE_ARRAY:
+						button_actions[subbed_action] = []
+						for button in data.result[action]:
+							var btnVal
+							if typeof(button) == TYPE_REAL:
+								if int(button) >= 0 and int(button) < JOY_BUTTON_MAX:
+									btnVal = int(button)
+								else:
+									printerr("Error reading bindings for action " + action + ": " + button + " is not a valid button number")
+							elif typeof(button) == TYPE_STRING:
+								if button in BUTTON_NAMES:
+									btnVal = BUTTON_NAMES[button]
+								else:
+									printerr("Error reading bindings for action " + action + ": " + button + " is not a valid button name")
+									continue
+							else:
+								printerr("Error reading bindings for action " + action + ": Expected string or number, got " + typeof(button))
+								continue
+							button_actions[subbed_action].append(btnVal)
+					else:
+						printerr("Value for action " + action + " must be a list of button")
+			else:
+				printerr("Error reading controller bindings: File contents is not a JSON Object")
+		else:
+			printerr("Error reading controller bindings: Unable to parse JSON")
+	else:
+		printerr("Error reading controller bindings: File does not exist or could not be read")
+
 
 func _process(_delta):
 	player = null
 	player = get_tree().current_scene.get_node_or_null("ViewportContainer/Viewport/Player")
 	if player == null:
 		return
-	if player.frozen != input_swapped:
-		var ev = InputEventJoypadButton.new()
-		ev.button_index = JOY_XBOX_A
-		ev.pressed = true
-		if input_swapped:
-			InputMap.action_erase_event("action", ev)
-		else:
-			InputMap.action_add_event("action", ev)
-		input_swapped = player.frozen
 
 	if not player.is_processing():
 		return
@@ -97,20 +172,10 @@ func camera_rotation(joy_axis : Vector2):
 		player.head.rotation_degrees = temp_rot
 
 func _input(event):
-	if event is InputEventJoypadButton:
-		if not event.pressed:
-			return
-		if event.button_index == JOY_XBOX_A:
-			var dialogue = get_tree().current_scene.get_node_or_null("ViewportContainer/dialogue")
-			if dialogue != null and player.frozen:
-				var ev = InputEventAction.new()
-				ev.action = "action"
-				ev.pressed = true
-				dialogue._input(ev)
-			elif player != null:
-				var ev = InputEventAction.new()
-				ev.action = "move_jump"
-				ev.pressed = true
-				player._input(ev)
+	if event.is_action_pressed("dialogue_advance"):
+		var dialogue = get_tree().current_scene.get_node_or_null("ViewportContainer/dialogue")
+		if dialogue != null and dialogue.catchClick:
+			dialogue.advance()
+			dialogue.catchClick = false
 
 			
